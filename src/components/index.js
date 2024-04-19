5a7629de6298fd52d9c1b50fd64e642c584de1b9
abhaydixit07 <abhaydixit227@gmail.com>
2024-04-20 01:51:02 +0530

    Add components

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import PopularProducts from "./PopularProducts";
+export {
+    Hero,
+    CustomerReviews,
+    Footer,
+    Services,
+    SuperQuality,
+    Subscribe,
+    SpecialOffers,
+    PopularProducts
+
+}
